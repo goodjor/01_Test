901be7a3dbc1a5e5f13c604250af1be63be9d46e
superhangz <1084627052@qq.com>
2024-11-27 09:57:37 +0800

    first commit test

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+
+int main(void) {
+    int a, b;
+    a = 1;
+    b = 3;
+    int c = a + b;
+    printf("c = %d \r\n", c);
+    printf("hello, world!\r\n");
+    printf("你好，世界\r\n");
+    return 0;
+}
+
